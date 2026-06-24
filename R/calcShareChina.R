3768f80f020b0a93de20ffe5a8d654cf094c11a4
hagento <hagento@pik-potsdam.de>
2026-06-24 14:21:00 +0200

    correct space_heating / water_heating split in China with IEA Future of Heat Pumps report

--- R/calcShareChina.R
+++ R/calcShareChina.R
@@ -1,21 +1,55 @@
 #' Calculate shares from final energy end-use demand for China
 #'
 #' The data is obtained from the 2021 IEA report "An Energy Sector Roadmap to Carbon
-#' Neutrality in China" from Figure 3.25
+#' Neutrality in China" from Figure 3.25 and 2024 IEA report "The Future of Heat Pumps
+#' in China" from Figure 1.6.
 #'
-#' @author Robin Hasse
+#' The latter report states in Box 1.1 that district heating has been incorrectly assigned
+#' to water heating by the IEA, thus significantly overestimating the demand and underestimating
+#' demand for space heating. They corrected their numbers using data from the
+#' Tsinghua University's Annual Report on China Building Energy Efficiency in 2022 which
+#' are used here to correct the space_heating/water_heating split.
+#'
+#' @author Robin Hasse, Hagen Tockhorn
 #'
 #' @importFrom madrat readSource toolCountryFill
 #' @importFrom magclass dimSums
 
 calcShareChina <- function() {
-  x <- readSource("IEA_China")
+  roadmapReportData <- readSource("IEA_China", subtype = "EnergySectorRoadmap") %>%
+    as.quitte(na.rm = TRUE)
+
+  heatPumpReportData <- readSource("IEA_China", subtype = "FutureOfHeatPumps") %>%
+    as.quitte(na.rm = TRUE)
+
+  heatSplit <- heatPumpReportData %>%
+    pivot_wider(names_from = "enduse", values_from = "value") %>%
+    mutate(spaceHeatShare = .data$space_heating / (.data$space_heating + .data$water_heating)) %>%
+    filter(.data$scenario %in% c("history", "APS")) %>%
+    select("region", "period", "spaceHeatShare") %>%
+    interpolate_missing_periods(period = unique(roadmapReportData$period),
+                                value = "spaceHeatShare",
+                                expand.values = TRUE)
+
+  x <- roadmapReportData %>%
+    left_join(heatSplit, by = c("region", "period")) %>%
+    group_by(across(-all_of(c("enduse", "value", "spaceHeatShare")))) %>%
+    mutate(totalHeat = sum(.data$value[.data$enduse %in% c("space_heating", "water_heating")])) %>%
+    ungroup() %>%
+    mutate(value = case_when(
+      .data$enduse == "space_heating" ~ .data$totalHeat * .data$spaceHeatShare,
+      .data$enduse == "water_heating" ~ .data$totalHeat * (1 - .data$spaceHeatShare),
+      .default = .data$value
+    )) %>%
+    select("region", "period", "enduse", "value") %>%
+    as.magpie()
+
   x <- x[, , "other", invert = TRUE]
   shares <- x / dimSums(x, "enduse")
   shares <- toolCountryFill(shares, verbosity = 2)
-  w <- toolCountryFill(x, 1, verbosity = 2)
+  weights <- toolCountryFill(x, 1, verbosity = 2)
   list(x = shares,
-       weight = w,
+       weight = weights,
        min = 0,
        max = 1,
        unit = "1",
--- R/readIEA_China.R
+++ R/readIEA_China.R
@@ -1,7 +1,10 @@
 #' Read final energy data by energy end-use for China
 #'
-#' The data is obtained from the 2021 IEA report "An Energy Sector Roadmap to Carbon
-#' Neutrality in China" from Figure 3.25
+#' The data is obtained from the either the 2021 IEA report "An Energy Sector Roadmap to Carbon
+#' Neutrality in China" from Figure 3.25 or the 2024 IEA report "The Future of Heat Pumps
+#' in China" from Figure 1.6.
+#'
+#' @param subtype indicates source to use
 #'
 #' @author Robin Hasse
 #'
@@ -9,9 +12,16 @@
 #' @importFrom magclass as.magpie
 #' @importFrom utils read.csv
 
-readIEA_China <- function() { #nolint object_name_linter
-  read.csv("figure3-25.csv") %>%
-    select("region", "period", "enduse", "value") %>%
-    as.magpie(spatial = "region", temporal = "period", datacol = "value",
-              tidy = FALSE)
+readIEA_China <- function(subtype = c("EnergySectorRoadmap", "FutureOfHeatPumps")) { #nolint object_name_linter
+  if (subtype == "EnergySectorRoadmap") {
+    read.csv("figure3-25.csv") %>%
+      select("region", "period", "enduse", "value") %>%
+      as.magpie(spatial = "region", temporal = "period", datacol = "value",
+                tidy = FALSE)
+  } else {
+    read.csv("figure1-6.csv") %>%
+      select("region", "period", "enduse", "scenario", "value") %>%
+      as.magpie(spatial = "region", temporal = "period", datacol = "value",
+                tidy = FALSE)
+  }
 }
